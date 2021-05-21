ee77f477696a56ce162e2e3681d56e27e7e2a21f
Dalila <dmberbes@gmail.com>
2021-05-21 19:29:36 -0400

    Missing import
    -Fixed missing import error in Institution class

--- ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/organization/entity/Institution.java
+++ ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/organization/entity/Institution.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Objects;
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
