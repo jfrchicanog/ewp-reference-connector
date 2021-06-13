46b445e52038293026a329aa7bef0a0876a611e8
francis <chicano@lcc.uma.es>
2021-06-13 21:38:23 +0200

    security extension

--- ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/organization/boundary/InstitutionsManifestEntry.java
+++ ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/organization/boundary/InstitutionsManifestEntry.java
@@ -1,7 +1,12 @@
 package eu.erasmuswithoutpaper.organization.boundary;
 
 import eu.erasmuswithoutpaper.api.architecture.ManifestApiEntryBase;
+import eu.erasmuswithoutpaper.api.client.auth.methods.cliauth.httpsig.CliauthHttpsig;
+import eu.erasmuswithoutpaper.api.client.auth.methods.cliauth.tlscert.CliauthTlscert;
+import eu.erasmuswithoutpaper.api.client.auth.methods.srvauth.httpsig.SrvauthHttpsig;
+import eu.erasmuswithoutpaper.api.client.auth.methods.srvauth.tlscert.SrvauthTlscert;
 import eu.erasmuswithoutpaper.api.institutions.Institutions;
+import eu.erasmuswithoutpaper.api.specs.sec.intro.HttpSecurityOptions;
 import eu.erasmuswithoutpaper.common.control.EwpConstants;
 import eu.erasmuswithoutpaper.common.control.GlobalProperties;
 import java.math.BigInteger;
@@ -26,6 +31,27 @@ public class InstitutionsManifestEntry implements ManifestEntryStrategy {
         
         LOGGER.info(institutions.getVersion());
         
+        HttpSecurityOptions httpSecurityOptions = new HttpSecurityOptions();
+        
+        HttpSecurityOptions.ClientAuthMethods clientAuthMethods = new HttpSecurityOptions.ClientAuthMethods();
+        
+        CliauthTlscert cliauthtlscert = new CliauthTlscert();
+        cliauthtlscert.setAllowsSelfSigned(true);
+        clientAuthMethods.getAny().add(cliauthtlscert);
+        
+//        clientAuthMethods.getAny().add(new Anonymous());
+        
+        clientAuthMethods.getAny().add(new CliauthHttpsig());
+        
+        httpSecurityOptions.setClientAuthMethods(clientAuthMethods);
+        
+        HttpSecurityOptions.ServerAuthMethods serverAuthMethods = new HttpSecurityOptions.ServerAuthMethods();
+        
+        serverAuthMethods.getAny().add(new SrvauthTlscert());
+        serverAuthMethods.getAny().add(new SrvauthHttpsig());
+        
+        httpSecurityOptions.setServerAuthMethods(serverAuthMethods);
+        institutions.setHttpSecurity(httpSecurityOptions);
         
         return institutions;
     }
