e796d2a59d68904617c9d990e8fe3083212511c8
francis <chicano@lcc.uma.es>
2021-06-19 12:26:03 +0200

    adding security to ounits

--- ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/organization/boundary/OrganizationUnitsManifestEntry.java
+++ ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/organization/boundary/OrganizationUnitsManifestEntry.java
@@ -1,7 +1,12 @@
 package eu.erasmuswithoutpaper.organization.boundary;
 
 import eu.erasmuswithoutpaper.api.architecture.ManifestApiEntryBase;
+import eu.erasmuswithoutpaper.api.client.auth.methods.cliauth.httpsig.CliauthHttpsig;
+import eu.erasmuswithoutpaper.api.client.auth.methods.cliauth.tlscert.CliauthTlscert;
+import eu.erasmuswithoutpaper.api.client.auth.methods.srvauth.httpsig.SrvauthHttpsig;
+import eu.erasmuswithoutpaper.api.client.auth.methods.srvauth.tlscert.SrvauthTlscert;
 import eu.erasmuswithoutpaper.api.ounits.OrganizationalUnits;
+import eu.erasmuswithoutpaper.api.specs.sec.intro.HttpSecurityOptions;
 import eu.erasmuswithoutpaper.common.control.EwpConstants;
 import eu.erasmuswithoutpaper.common.control.GlobalProperties;
 import java.math.BigInteger;
@@ -19,6 +24,29 @@ public class OrganizationUnitsManifestEntry implements ManifestEntryStrategy {
         organizationalUnits.setUrl(baseUri + "ounits");
         organizationalUnits.setMaxOunitIds(BigInteger.valueOf(globalProperties.getMaxOunitsIds()));
         organizationalUnits.setMaxOunitCodes(BigInteger.valueOf(globalProperties.getMaxOunitsIds()));
+        
+        HttpSecurityOptions httpSecurityOptions = new HttpSecurityOptions();
+        
+        HttpSecurityOptions.ClientAuthMethods clientAuthMethods = new HttpSecurityOptions.ClientAuthMethods();
+        
+        CliauthTlscert cliauthtlscert = new CliauthTlscert();
+        cliauthtlscert.setAllowsSelfSigned(true);
+        clientAuthMethods.getAny().add(cliauthtlscert);
+        
+//        clientAuthMethods.getAny().add(new Anonymous());
+        
+        clientAuthMethods.getAny().add(new CliauthHttpsig());
+        
+        httpSecurityOptions.setClientAuthMethods(clientAuthMethods);
+        
+        HttpSecurityOptions.ServerAuthMethods serverAuthMethods = new HttpSecurityOptions.ServerAuthMethods();
+        
+        serverAuthMethods.getAny().add(new SrvauthTlscert());
+        serverAuthMethods.getAny().add(new SrvauthHttpsig());
+        
+        httpSecurityOptions.setServerAuthMethods(serverAuthMethods);
+        organizationalUnits.setHttpSecurity(httpSecurityOptions);
+        
         return organizationalUnits;
     }
 }
