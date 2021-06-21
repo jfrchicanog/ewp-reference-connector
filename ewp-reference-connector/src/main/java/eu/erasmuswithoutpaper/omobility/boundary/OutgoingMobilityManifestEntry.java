78b6b2043682be00128c6edf82c02b25f140b6dd
Dalila <dmberbes@gmail.com>
2021-06-20 23:12:36 -0400

    Mobility security policies

--- ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/omobility/boundary/OutgoingMobilityManifestEntry.java
+++ ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/omobility/boundary/OutgoingMobilityManifestEntry.java
@@ -1,13 +1,20 @@
 package eu.erasmuswithoutpaper.omobility.boundary;
 
+import java.math.BigInteger;
+
+import javax.inject.Inject;
+
 import eu.erasmuswithoutpaper.api.architecture.Empty;
 import eu.erasmuswithoutpaper.api.architecture.ManifestApiEntryBase;
+import eu.erasmuswithoutpaper.api.client.auth.methods.cliauth.httpsig.CliauthHttpsig;
+import eu.erasmuswithoutpaper.api.client.auth.methods.cliauth.tlscert.CliauthTlscert;
+import eu.erasmuswithoutpaper.api.client.auth.methods.srvauth.httpsig.SrvauthHttpsig;
+import eu.erasmuswithoutpaper.api.client.auth.methods.srvauth.tlscert.SrvauthTlscert;
 import eu.erasmuswithoutpaper.api.omobilities.Omobilities;
+import eu.erasmuswithoutpaper.api.specs.sec.intro.HttpSecurityOptions;
+import eu.erasmuswithoutpaper.common.boundary.ManifestEntryStrategy;
 import eu.erasmuswithoutpaper.common.control.EwpConstants;
 import eu.erasmuswithoutpaper.common.control.GlobalProperties;
-import java.math.BigInteger;
-import javax.inject.Inject;
-import eu.erasmuswithoutpaper.common.boundary.ManifestEntryStrategy;
 
 public class OutgoingMobilityManifestEntry implements ManifestEntryStrategy {
     @Inject
@@ -27,6 +34,26 @@ public class OutgoingMobilityManifestEntry implements ManifestEntryStrategy {
         supportedUpdateTypes.setApproveComponentsStudiedDraftV1(new Empty());
         supportedUpdateTypes.setUpdateComponentsStudiedV1(new Empty());
         mobilities.setSupportedUpdateTypes(supportedUpdateTypes);
+        
+        HttpSecurityOptions httpSecurityOptions = new HttpSecurityOptions();
+        
+        HttpSecurityOptions.ClientAuthMethods clientAuthMethods = new HttpSecurityOptions.ClientAuthMethods();
+        
+        CliauthTlscert cliauthtlscert = new CliauthTlscert();
+        cliauthtlscert.setAllowsSelfSigned(true);
+        clientAuthMethods.getAny().add(cliauthtlscert);
+        
+        clientAuthMethods.getAny().add(new CliauthHttpsig());
+        
+        httpSecurityOptions.setClientAuthMethods(clientAuthMethods);
+        
+        HttpSecurityOptions.ServerAuthMethods serverAuthMethods = new HttpSecurityOptions.ServerAuthMethods();
+        
+        serverAuthMethods.getAny().add(new SrvauthTlscert());
+        serverAuthMethods.getAny().add(new SrvauthHttpsig());
+        
+        httpSecurityOptions.setServerAuthMethods(serverAuthMethods);
+        mobilities.setHttpSecurity(httpSecurityOptions);
         return mobilities;
     }
 }
