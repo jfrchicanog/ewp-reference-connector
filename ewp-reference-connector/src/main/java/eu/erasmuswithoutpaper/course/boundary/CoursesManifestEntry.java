3a839f7f843d0e35bbaa942a25fb924557cc51d7
Francisco Chicano <chicano@lcc.uma.es>
2021-10-24 09:06:13 +0200

    updated security mechanisms

--- ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/course/boundary/CoursesManifestEntry.java
+++ ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/course/boundary/CoursesManifestEntry.java
@@ -1,7 +1,12 @@
 package eu.erasmuswithoutpaper.course.boundary;
 
 import eu.erasmuswithoutpaper.api.architecture.ManifestApiEntryBase;
+import eu.erasmuswithoutpaper.api.client.auth.methods.cliauth.httpsig.CliauthHttpsig;
+import eu.erasmuswithoutpaper.api.client.auth.methods.cliauth.tlscert.CliauthTlscert;
+import eu.erasmuswithoutpaper.api.client.auth.methods.srvauth.httpsig.SrvauthHttpsig;
+import eu.erasmuswithoutpaper.api.client.auth.methods.srvauth.tlscert.SrvauthTlscert;
 import eu.erasmuswithoutpaper.api.courses.Courses;
+import eu.erasmuswithoutpaper.api.specs.sec.intro.HttpSecurityOptions;
 import eu.erasmuswithoutpaper.common.control.EwpConstants;
 import eu.erasmuswithoutpaper.common.control.GlobalProperties;
 import java.math.BigInteger;
@@ -19,6 +24,30 @@ public class CoursesManifestEntry implements ManifestEntryStrategy {
         courses.setUrl(baseUri + "courses");
         courses.setMaxLosIds(BigInteger.valueOf(globalProperties.getMaxLosIds()));
         courses.setMaxLosCodes(BigInteger.valueOf(globalProperties.getMaxLosIds()));
+        
+        HttpSecurityOptions httpSecurityOptions = new HttpSecurityOptions();
+        
+        HttpSecurityOptions.ClientAuthMethods clientAuthMethods = new HttpSecurityOptions.ClientAuthMethods();
+        
+        CliauthTlscert cliauthtlscert = new CliauthTlscert();
+        cliauthtlscert.setAllowsSelfSigned(true);
+        clientAuthMethods.getAny().add(cliauthtlscert);
+        
+//        clientAuthMethods.getAny().add(new Anonymous());
+        
+        clientAuthMethods.getAny().add(new CliauthHttpsig());
+        
+        httpSecurityOptions.setClientAuthMethods(clientAuthMethods);
+        
+        HttpSecurityOptions.ServerAuthMethods serverAuthMethods = new HttpSecurityOptions.ServerAuthMethods();
+        
+        serverAuthMethods.getAny().add(new SrvauthTlscert());
+        serverAuthMethods.getAny().add(new SrvauthHttpsig());
+        
+        httpSecurityOptions.setServerAuthMethods(serverAuthMethods);
+        
+        courses.setHttpSecurity(httpSecurityOptions);
+        
         return courses;
     }
 }
--- ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/imobility/boundary/IncomingMobilityManifestEntry.java
+++ ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/imobility/boundary/IncomingMobilityManifestEntry.java
@@ -2,7 +2,12 @@ package eu.erasmuswithoutpaper.imobility.boundary;
 
 import eu.erasmuswithoutpaper.api.architecture.Empty;
 import eu.erasmuswithoutpaper.api.architecture.ManifestApiEntryBase;
+import eu.erasmuswithoutpaper.api.client.auth.methods.cliauth.httpsig.CliauthHttpsig;
+import eu.erasmuswithoutpaper.api.client.auth.methods.cliauth.tlscert.CliauthTlscert;
+import eu.erasmuswithoutpaper.api.client.auth.methods.srvauth.httpsig.SrvauthHttpsig;
+import eu.erasmuswithoutpaper.api.client.auth.methods.srvauth.tlscert.SrvauthTlscert;
 import eu.erasmuswithoutpaper.api.imobilities.Imobilities;
+import eu.erasmuswithoutpaper.api.specs.sec.intro.HttpSecurityOptions;
 import eu.erasmuswithoutpaper.common.control.EwpConstants;
 import eu.erasmuswithoutpaper.common.control.GlobalProperties;
 import java.math.BigInteger;
@@ -21,6 +26,30 @@ public class IncomingMobilityManifestEntry implements ManifestEntryStrategy {
         mobilities.setMaxOmobilityIds(BigInteger.valueOf(globalProperties.getMaxMobilityIds()));
 
         mobilities.setSendsNotifications(new Empty());
+        
+        HttpSecurityOptions httpSecurityOptions = new HttpSecurityOptions();
+        
+        HttpSecurityOptions.ClientAuthMethods clientAuthMethods = new HttpSecurityOptions.ClientAuthMethods();
+        
+        CliauthTlscert cliauthtlscert = new CliauthTlscert();
+        cliauthtlscert.setAllowsSelfSigned(true);
+        clientAuthMethods.getAny().add(cliauthtlscert);
+        
+//        clientAuthMethods.getAny().add(new Anonymous());
+        
+        clientAuthMethods.getAny().add(new CliauthHttpsig());
+        
+        httpSecurityOptions.setClientAuthMethods(clientAuthMethods);
+        
+        HttpSecurityOptions.ServerAuthMethods serverAuthMethods = new HttpSecurityOptions.ServerAuthMethods();
+        
+        serverAuthMethods.getAny().add(new SrvauthTlscert());
+        serverAuthMethods.getAny().add(new SrvauthHttpsig());
+        
+        httpSecurityOptions.setServerAuthMethods(serverAuthMethods);
+        
+        mobilities.setHttpSecurity(httpSecurityOptions);
+        
         return mobilities;
     }
 }
