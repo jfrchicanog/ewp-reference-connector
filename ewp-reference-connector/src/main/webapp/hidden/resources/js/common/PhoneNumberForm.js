a428f2f3c331b6e21976779de67215ec6f5eb203
Francisco Chicano <chicano@lcc.uma.es>
2021-11-09 13:55:20 +0100

    frontend fixed

--- ewp-reference-connector/src/main/webapp/hidden/resources/js/common/PhoneNumberForm.js
+++
@@ -1,9 +0,0 @@
-angular.module('connector').directive('phoneNumberForm', function() {
-        return {
-            restrict: 'A',
-            scope: {
-                phoneNumber: '=phoneNumberForm'
-            },
-            templateUrl: 'templates/phone_number_form.html'
-        };
-    });
