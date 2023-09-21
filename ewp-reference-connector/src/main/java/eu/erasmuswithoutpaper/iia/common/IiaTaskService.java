ca22c0aac7de5089bdc60fd1dc94d44333ec0c56
Moritz Baader <moritzbaader@gmail.com>
2023-09-21 12:19:24 +0200

    Test notifications

--- ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/iia/common/IiaTaskService.java
+++ ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/iia/common/IiaTaskService.java
@@ -215,8 +215,8 @@ public class IiaTaskService {
             }
         } catch (InterruptedException | ExecutionException | IOException e) {
 
-            logger.error("The task was interrupted! " + e.getMessage());
-            logger.error(e.toString());
+            logger.error("The task was interrupted TEST! " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
