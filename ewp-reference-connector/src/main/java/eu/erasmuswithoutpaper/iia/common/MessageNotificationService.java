6c3d2a1cf1ece0e8f089c262d24deb820b75b007
Moritz Baader <moritzbaader@gmail.com>
2023-09-22 12:10:15 +0200

    Test notifications

--- ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/iia/common/MessageNotificationService.java
+++ ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/iia/common/MessageNotificationService.java
@@ -22,18 +22,24 @@ public class MessageNotificationService {
 
     public static Response addApprovalNotification(String url, String msg, String token) {
 
-        ClientBuilder clientBuilder = ClientBuilder.newBuilder();/*
+        ClientBuilder clientBuilder = ClientBuilder.newBuilder();
         try {
             clientBuilder.sslContext(SSLContext.getDefault());
         } catch (NoSuchAlgorithmException e1) {
             logger.error("Error setting ssl context! " + e1.getMessage());
-        }*/
+        }
 
         WebTarget target = clientBuilder.build().target(url);
-        //target.property("http.autoredirect", true);
+        target.property("http.autoredirect", true);
 
         Invocation.Builder postBuilder = target.request().header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_TYPE);
         postBuilder = postBuilder.header("Authorization", token);
+        
+        logger.info("\n\n------------------------------------");
+        logger.info("url: " + url);
+        logger.info("msg: " + msg);
+        logger.info("toke: " + token);
+        logger.info("\n\n------------------------------------\n\n");
         
         Response response = postBuilder.post(Entity.json(msg));
 
