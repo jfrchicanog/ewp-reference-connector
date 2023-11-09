99374a1619a0821863e2558681454c80f0220963
Moritz Baader <moritzbaader@gmail.com>
2023-11-09 23:59:24 +0100

    Test notificaciones 10

--- ewp-connector-api/target/generated-sources/jaxb/eu/erasmuswithoutpaper/api/imobilities/package-info.java
+++ ewp-connector-api/target/generated-sources/jaxb/eu/erasmuswithoutpaper/api/imobilities/package-info.java
@@ -2,7 +2,7 @@
 // Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.3.2 generiert 
 // Siehe <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2023.11.09 um 10:21:24 PM CET 
+// Generiert: 2023.11.09 um 11:57:55 PM CET 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "https://github.com/erasmus-without-paper/ewp-specs-api-imobilities/blob/stable-v1/manifest-entry.xsd", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
--- ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/common/control/GlobalProperties.java
+++ ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/common/control/GlobalProperties.java
@@ -25,9 +25,9 @@ public class GlobalProperties {
     String defaultAlgoriaToken = "675701176db0293a8cac23814481f8e50b320fbd";
     
     //String defaultAlgoriaApprovalURL = "https://relacionesi.uma.es/algoria/ewp_approved_agreement_notifications/";
-    String defaultAlgoriaApprovalURL = "https://relacionesi-test.uma.es/algoria/ewp_approved_agreement_notifications/";
-    String defaultAlgoriaModifyURL = "https://relacionesi-test.uma.es/algoria/ewp_modified_agreement_notifications/";
-    String defaultAlgoriaAuthorizationToken = "Token 83796a548ff904efcc2811ba2cf6d4049009d149";
+    String defaultAlgoriaApprovalURL = "http://relacionesi-test.uma.es/algoria/ewp_approved_agreement_notifications/";
+    String defaultAlgoriaModifyURL = "http://relacionesi-test.uma.es/algoria/ewp_modified_agreement_notifications/";
+    String defaultAlgoriaAuthorizationToken = "Token d6cd15f2e78e02148c11f4e12a286aead4dbdf10";
 
     public GlobalProperties() {
         loadProperties();
