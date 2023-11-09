423c459375883d91189f635f706985dcf1ac857a
MoritzB112 <moritzbaader@gmail.com>
2023-11-09 18:36:06 +0100

    Mensajes a algoria

--- ewp-connector-api/target/generated-sources/jaxb/eu/erasmuswithoutpaper/api/architecture/package-info.java
+++ ewp-connector-api/target/generated-sources/jaxb/eu/erasmuswithoutpaper/api/architecture/package-info.java
@@ -2,7 +2,7 @@
 // Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.3.2 generiert 
 // Siehe <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2023.11.09 um 10:03:16 AM CET 
+// Generiert: 2023.11.09 um 06:35:00 PM CET 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "https://github.com/erasmus-without-paper/ewp-specs-architecture/blob/stable-v1/common-types.xsd", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
