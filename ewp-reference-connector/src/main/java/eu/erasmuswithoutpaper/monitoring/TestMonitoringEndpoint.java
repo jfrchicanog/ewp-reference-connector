a3e963a99a49e014f0a45d6acbc734745915b90e
Moritz Baader <moritzbaader@gmail.com>
2024-01-30 09:03:49 +0100

    Test Monitoring

--- ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/monitoring/TestMonitoringEndpoint.java
+++ ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/monitoring/TestMonitoringEndpoint.java
@@ -4,12 +4,31 @@
  */
 package eu.erasmuswithoutpaper.monitoring;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.apache.http.HttpRequest;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 /**
  *
@@ -18,17 +37,54 @@ import javax.ws.rs.core.Response;
 @Stateless
 @Path("testMonitoring")
 public class TestMonitoringEndpoint {
-    
+
     @GET
     @Produces(MediaType.APPLICATION_FORM_URLENCODED)
-    public Response testMonitoring() {
+    public Response testMonitoring() throws Exception {
         MonitoringParams mp = new MonitoringParams();
-        mp.setServerHeiId("test.uma.es");
-        mp.setApiName("omobility-las");
-        mp.setEndpointName("get");
-        mp.setHttpCode("500");
-        mp.setServerMessage("Mensaje error Server");
-        
+
+        // Build the request
+        HttpPost request = new HttpPost("http://your-server/testMonitoring");
+        request.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        List<NameValuePair> urlParameters = new ArrayList<>();
+        urlParameters.add(new BasicNameValuePair("server_hei_id", "test.uma.es"));
+        urlParameters.add(new BasicNameValuePair("api_name", "omobility-las"));
+        urlParameters.add(new BasicNameValuePair("endpoint_name", "get"));
+        urlParameters.add(new BasicNameValuePair("http_code", "500"));
+        urlParameters.add(new BasicNameValuePair("server_message", "Mensaje error Server"));
+
+        request.setEntity(new UrlEncodedFormEntity(urlParameters));
+
+        // Send the request and get the response
+        try (CloseableHttpClient httpClient = HttpClients.createDefault();
+             CloseableHttpResponse response = httpClient.execute(request)){
+            ObjectMapper objectMapper = new ObjectMapper();
+            
+            String result = EntityUtils.toString(response.getEntity());
+            mp = objectMapper.readValue(result, MonitoringParams.class);
+        }
+
+        return Response.ok(mp).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response testMonitoring(
+            @FormParam("server_hei_id") String serverHeiId,
+            @FormParam("api_name") String apiName,
+            @FormParam("endpoint_name") String endpointName,
+            @FormParam("http_code") String httpCode,
+            @FormParam("server_message") String serverMessage
+    ) {
+        MonitoringParams mp = new MonitoringParams();
+        mp.setServerHeiId(serverHeiId);
+        mp.setApiName(apiName);
+        mp.setEndpointName(endpointName);
+        mp.setHttpCode(httpCode);
+        mp.setServerMessage(serverMessage);
+
+        // Process the data as needed
         return Response.ok(mp).build();
     }
 }
