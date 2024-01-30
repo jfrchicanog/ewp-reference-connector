1558187dc1ca561ffc93166b49395283cce26536
Moritz Baader <moritzbaader@gmail.com>
2024-01-30 12:41:24 +0100

    Test 5 Monitoring

--- ewp-connector-api/target/generated-sources/jaxb/eu/erasmuswithoutpaper/api/institutions/package-info.java
+++ ewp-connector-api/target/generated-sources/jaxb/eu/erasmuswithoutpaper/api/institutions/package-info.java
@@ -2,7 +2,7 @@
 // Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.3.2 generiert 
 // Siehe <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2024.01.30 um 12:28:17 PM CET 
+// Generiert: 2024.01.30 um 12:40:20 PM CET 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "https://github.com/erasmus-without-paper/ewp-specs-api-institutions/tree/stable-v2", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
