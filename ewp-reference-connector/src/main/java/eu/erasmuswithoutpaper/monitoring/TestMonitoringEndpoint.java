723c17a9a0118ebbf58681ccd69d36db6d7dda1b
Moritz Baader <moritzbaader@gmail.com>
2024-01-30 10:09:03 +0100

    Test 2 Monitoring

--- ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/monitoring/TestMonitoringEndpoint.java
+++ ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/monitoring/TestMonitoringEndpoint.java
@@ -78,7 +78,7 @@ public class TestMonitoringEndpoint {
         cr.setHeiId("uma.es");
         cr.setHttpsec(false);
         cr.setMethod(HttpMethodEnum.POST);
-        cr.setUrl("https://stats.erasmuswithoutpaper.eu");
+        cr.setUrl("http://stats.erasmuswithoutpaper.eu");
         Map<String, List<String>> unknownFields = new HashMap<>();
         unknownFields.put("server_hei_id", Arrays.asList("test.uma.es"));
         unknownFields.put("api_name", Arrays.asList("omobility-las"));
