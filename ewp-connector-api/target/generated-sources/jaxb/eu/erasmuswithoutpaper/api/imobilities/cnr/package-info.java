801bb8a0ff005233be9c0e85d819fe3b79746374
MoritzB112 <moritzbaader@gmail.com>
2024-02-02 16:49:48 +0100

    Test notify

--- ewp-connector-api/target/generated-sources/jaxb/eu/erasmuswithoutpaper/api/imobilities/cnr/package-info.java
+++ ewp-connector-api/target/generated-sources/jaxb/eu/erasmuswithoutpaper/api/imobilities/cnr/package-info.java
@@ -2,7 +2,7 @@
 // Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.3.2 generiert 
 // Siehe <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2024.02.02 um 01:55:25 PM CET 
+// Generiert: 2024.02.02 um 04:49:08 PM CET 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "https://github.com/erasmus-without-paper/ewp-specs-api-imobility-cnr/blob/stable-v1/manifest-entry.xsd", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
