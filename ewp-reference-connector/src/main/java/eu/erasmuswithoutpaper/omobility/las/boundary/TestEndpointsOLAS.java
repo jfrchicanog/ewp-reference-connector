ae6213df53733cd1559437f746ad0a15975add42
MoritzB112 <moritzbaader@gmail.com>
2024-03-04 18:48:43 +0100

    Test

--- ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/omobility/las/boundary/TestEndpointsOLAS.java
+++ ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/omobility/las/boundary/TestEndpointsOLAS.java
@@ -1,20 +1,29 @@
 package eu.erasmuswithoutpaper.omobility.las.boundary;
 
 import eu.erasmuswithoutpaper.api.omobilities.las.OmobilityLas;
+import eu.erasmuswithoutpaper.api.omobilities.las.endpoints.LearningAgreement;
+import eu.erasmuswithoutpaper.api.omobilities.las.endpoints.OmobilityLasGetResponse;
+import eu.erasmuswithoutpaper.omobility.las.control.OutgoingMobilityLearningAgreementsConverter;
 import eu.erasmuswithoutpaper.omobility.las.entity.OlearningAgreement;
+import eu.erasmuswithoutpaper.omobility.las.entity.Student;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPublicKey;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.stream.Collectors;
 
 @Stateless
 @Path("omobilities/las/test")
@@ -23,15 +32,21 @@ public class TestEndpointsOLAS {
     @PersistenceContext(unitName = "connector")
     EntityManager em;
 
+    @Inject
+    OutgoingMobilityLearningAgreementsConverter converter;
+
     @GET
     @Path("")
-    public Response hello() {
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<OlearningAgreement> cq = cb.createQuery(OlearningAgreement.class);
-        Root<OlearningAgreement> rootEntry = cq.from(OlearningAgreement.class);
-        CriteriaQuery<OlearningAgreement> all = cq.select(rootEntry);
-        TypedQuery<OlearningAgreement> allQuery = em.createQuery(all);
-        return Response.ok(allQuery.getResultList()).build();
+    public Response hello(@QueryParam("sending_hei_id") String sendingHeiId, @QueryParam("omobility_id") List<String> mobilityIdList) {
+        OmobilityLasGetResponse response = new OmobilityLasGetResponse();
+        List<OlearningAgreement> omobilityLasList = em.createNamedQuery(OlearningAgreement.findBySendingHeiId).setParameter("sendingHeiId", sendingHeiId).getResultList();
+
+        if (!omobilityLasList.isEmpty()) {
+
+            response.getLa().addAll(omobilitiesLas(omobilityLasList, mobilityIdList));
+        }
+
+        return javax.ws.rs.core.Response.ok(response).build();
 
     }
 
@@ -47,4 +62,68 @@ public class TestEndpointsOLAS {
 
         return Response.ok(em.find(OlearningAgreement.class, olearningAgreement.getId())).build();
     }
+
+    private List<LearningAgreement> omobilitiesLas(List<OlearningAgreement> omobilityLasList, List<String> omobilityLasIdList) {
+        List<LearningAgreement> omobilitiesLas = new ArrayList<>();
+        omobilityLasList.stream().forEachOrdered((m) -> {
+            if (omobilityLasIdList.contains(m.getId())) {
+                omobilitiesLas.add(converter.convertToLearningAgreements(m));
+            }
+        });
+
+        return omobilitiesLas;
+    }
+
+    private List<String> omobilityLasIds(List<OlearningAgreement> lasList, List<String> receivingHeiIdList) {
+        List<String> omobilityLasIds = new ArrayList<>();
+
+        lasList.stream().forEachOrdered((m) -> {
+            if (receivingHeiIdList.isEmpty() || receivingHeiIdList.contains(m.getReceivingHei())) {
+                omobilityLasIds.add(m.getId());
+            }
+        });
+
+        return omobilityLasIds;
+    }
+
+    BiPredicate<OlearningAgreement, String> anyMatchReceivingAcademicYear = new BiPredicate<OlearningAgreement, String>() {
+        @Override
+        public boolean test(OlearningAgreement omobility, String receiving_academic_year_id) {
+            return receiving_academic_year_id.equals(omobility.getReceivingAcademicTermEwpId());
+        }
+    };
+
+    BiPredicate<OlearningAgreement, String> anyMatchSpecifiedStudent = new BiPredicate<OlearningAgreement, String>() {
+        @Override
+        public boolean test(OlearningAgreement omobility, String global_id) {
+            Student student = omobility.getChangesProposal().getStudent();
+
+            return global_id.equals(student.getGlobalId());
+        }
+    };
+
+    BiPredicate<OlearningAgreement, String> anyMatchSpecifiedType = new BiPredicate<OlearningAgreement, String>() {
+        @Override
+        public boolean test(OlearningAgreement omobility, String mobilityType) {
+
+            if (MobilityType.BLENDED.value().equals(mobilityType)) {
+
+                return omobility.getFirstVersion().getBlendedMobilityComponents() != null && !omobility.getFirstVersion().getBlendedMobilityComponents().isEmpty();
+            } else if (MobilityType.DOCTORAL.value().equals(mobilityType)) {
+
+                return omobility.getFirstVersion().getShortTermDoctoralComponents() != null && !omobility.getFirstVersion().getShortTermDoctoralComponents().isEmpty();
+            } else if (MobilityType.SEMESTRE.value().equals(mobilityType)) {
+
+                if (omobility.getFirstVersion() != null) {
+
+                    return (omobility.getFirstVersion().getComponentsStudied() != null && !omobility.getFirstVersion().getComponentsStudied().isEmpty());
+                } else if (omobility.getApprovedChanges() != null) {
+
+                    return (omobility.getApprovedChanges().getComponentsStudied() != null && !omobility.getApprovedChanges().getComponentsStudied().isEmpty());
+                }
+            }
+
+            return false;
+        }
+    };
 }
