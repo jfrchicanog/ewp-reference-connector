494689a80ff356fe921df96f06cf81cb5b10ec39
MoritzB112 <moritzbaader@gmail.com>
2024-03-12 13:26:36 +0100

    HASH UTILS logs

--- ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/common/control/HashUtils.java
+++ ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/common/control/HashUtils.java
@@ -1,6 +1,7 @@
 package eu.erasmuswithoutpaper.common.control;
 
 import eu.erasmuswithoutpaper.api.iias.endpoints.IiasGetResponse;
+import eu.erasmuswithoutpaper.iia.boundary.AuxIiaThread;
 import org.w3c.dom.Document;
 
 import javax.ejb.Singleton;
@@ -19,8 +20,12 @@ import java.io.*;
 @Singleton
 public class HashUtils {
 
+    private static final java.util.logging.Logger LOG = java.util.logging.Logger.getLogger(HashUtils.class.getCanonicalName());
+
     public byte[] readXMLFileToByteArray(String filePath) throws IOException {
+        LOG.fine("HASH UTILS: start reading file to byte array");
         File file = new File(filePath);
+        LOG.fine("HASH UTILS: file path: " + file.getAbsolutePath());
         FileInputStream fis = new FileInputStream(file);
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         byte[] buffer = new byte[1024];
@@ -35,10 +40,13 @@ public class HashUtils {
             bos.close();
         }
 
+        LOG.fine("HASH UTILS: file to byte array conversion finished");
+
         return bos.toByteArray();
     }
 
     public byte[] convertObjectToByteArray(IiasGetResponse.Iia object) throws JAXBException, IOException {
+        HashUtils.LOG.fine("HASH UTILS: start iias object to byte array conversion");
         // Create JAXBContext
         JAXBContext jaxbContext = JAXBContext.newInstance(object.getClass());
 
@@ -50,11 +58,14 @@ public class HashUtils {
         ByteArrayOutputStream xmlOutputStream = new ByteArrayOutputStream();
         marshaller.marshal(object, xmlOutputStream);
 
+        LOG.fine("HASH UTILS: iias object to byte array conversion finished");
+
         // Convert XML to byte array
         return xmlOutputStream.toByteArray();
     }
 
     public String getXmlTransformed(IiasGetResponse.Iia iia) throws Exception {
+        LOG.fine("HASH UTILS: start transformation");
         byte[] xmlBytes = convertObjectToByteArray(iia);
         byte[] xsltBytes = readXMLFileToByteArray("META-INF/transform_version_7.xsl");
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
@@ -69,6 +80,8 @@ public class HashUtils {
         ByteArrayOutputStream output = new ByteArrayOutputStream();
 
         transformer.transform(new DOMSource(document), new StreamResult(output));
+
+        LOG.fine("HASH UTILS: transformation finished");
 
         return output.toString();
     }
