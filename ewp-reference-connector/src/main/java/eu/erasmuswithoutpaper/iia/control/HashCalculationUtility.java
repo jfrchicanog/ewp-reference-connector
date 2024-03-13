0c3bc88a64ee003dc68346a751a819ca18788b11
MoritzB112 <moritzbaader@gmail.com>
2024-03-13 12:33:48 +0100

    Algoritmo nuevo de calculo de hash implmentado

--- ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/iia/control/HashCalculationUtility.java
+++ ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/iia/control/HashCalculationUtility.java
@@ -82,9 +82,22 @@ public class HashCalculationUtility {
 		StringReader reader = new StringReader(xmlString);
 		HashClass iias = (HashClass) unmarshaller.unmarshal(reader);
 
-		LOG.fine("HASH UTILS: text to hash: " + iias.getIia().getTextToHash());
-		LOG.fine("HASH UTILS: text to hash byte array: " + Arrays.toString(iias.getIia().getTextToHash().getBytes(StandardCharsets.UTF_8)));
-		return getSHA256(iias.getIia().getTextToHash().getBytes(StandardCharsets.UTF_8));
+		MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+		return bytesToHex(digest.digest(
+				iias.getIia().getTextToHash().getBytes(StandardCharsets.UTF_8)));
+	}
+
+	private static String bytesToHex(byte[] hash) {
+		StringBuilder hexString = new StringBuilder(2 * hash.length);
+		for (int i = 0; i < hash.length; i++) {
+			String hex = Integer.toHexString(0xff & hash[i]);
+			if(hex.length() == 1) {
+				hexString.append('0');
+			}
+			hexString.append(hex);
+		}
+		return hexString.toString();
 	}
 
 	private static byte[] convertObjectToByteArray(IiasGetResponse object) throws JAXBException, IOException {
