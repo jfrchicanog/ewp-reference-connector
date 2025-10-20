1a76743653066d3f9e45d8eef3093698d4fdd605
MoritzB112 <moritzbaader@gmail.com>
2025-10-20 11:08:19 +0200

    sync-partner test

--- ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/omobility/las/boundary/GuiOutgoingMobilityLearningAgreementsResourceREAL.java
+++ ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/omobility/las/boundary/GuiOutgoingMobilityLearningAgreementsResourceREAL.java
@@ -689,4 +689,16 @@ public class GuiOutgoingMobilityLearningAgreementsResourceREAL {
 
         return javax.ws.rs.core.Response.ok(entity).build();
     }
+
+    @GET
+    @Path("by-partner")
+    public Response hello(@QueryParam("sending_hei_id") String sendingHeiId, @QueryParam("omobility_id") String id) {
+        OmobilityLasGetResponse response = new OmobilityLasGetResponse();
+        OlearningAgreement olearningAgreement = learningAgreementEJB.findBySendingHeiIdAndOmobilityId(sendingHeiId, id);
+        if (olearningAgreement != null) {
+
+            response.getLa().add(converter.convertToLearningAgreements(olearningAgreement));
+        }
+        return Response.ok(response).build();
+    }
 }
