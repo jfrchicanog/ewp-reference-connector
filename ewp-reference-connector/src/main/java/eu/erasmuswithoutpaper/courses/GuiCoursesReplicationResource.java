f1f69c5ffc6310300fe759ace44b6fb4a68f0fcb
MoritzB112 <moritzbaader@gmail.com>
2025-12-03 13:32:24 +0100

    Olas receiving_academic_year_id index

--- ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/courses/GuiCoursesReplicationResource.java
+++ ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/courses/GuiCoursesReplicationResource.java
@@ -1,5 +1,6 @@
 package eu.erasmuswithoutpaper.courses;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import eu.erasmuswithoutpaper.api.courses.replication.CourseReplicationResponse;
 import eu.erasmuswithoutpaper.common.boundary.ClientRequest;
 import eu.erasmuswithoutpaper.common.boundary.ClientResponse;
@@ -7,6 +8,11 @@ import eu.erasmuswithoutpaper.common.boundary.HttpMethodEnum;
 import eu.erasmuswithoutpaper.common.boundary.ParamsClass;
 import eu.erasmuswithoutpaper.common.control.RegistryClient;
 import eu.erasmuswithoutpaper.common.control.RestClient;
+import eu.erasmuswithoutpaper.courses.dto.AlgoriaLOApiResponse;
+import eu.erasmuswithoutpaper.error.control.EwpWebApplicationException;
+import eu.erasmuswithoutpaper.iia.common.AlgoriaTaskEnum;
+import eu.erasmuswithoutpaper.iia.common.AlgoriaTaskService;
+import eu.erasmuswithoutpaper.iia.common.AlgoriaTaskTypeEnum;
 import eu.erasmuswithoutpaper.monitoring.SendMonitoringService;
 
 import javax.inject.Inject;
@@ -19,6 +25,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @Path("courses/replication")
 public class GuiCoursesReplicationResource {
@@ -70,6 +77,33 @@ public class GuiCoursesReplicationResource {
         }
 
         return Response.ok(responseEnity).build();
+    }
+
+    @GET()
+    @Path("own")
+    @Produces("application/json")
+    public Response getIiaCoursesOwn(@QueryParam("heiId") String hei_id, @QueryParam("modifiedSince") List<String> modified_since) {
+        CourseReplicationResponse response = new CourseReplicationResponse();
+
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("hei_id", hei_id);
+        if (modified_since != null && !modified_since.isEmpty()) {
+            queryParams.put("modified_since", modified_since.get(0).substring(0, 10));
+        }
+        try {
+            Response resp = AlgoriaTaskService.sendRequest(AlgoriaTaskTypeEnum.COURSES, AlgoriaTaskEnum.GET_LIST, queryParams, new HashMap<>());
+            if (resp.getStatus() != Response.Status.OK.getStatusCode()) {
+                LOG.fine("Error fetching data from Algoria: " + resp.getStatusInfo().toString());
+                throw new EwpWebApplicationException("Error fetching data: " + resp.getStatusInfo().toString(), Response.Status.INTERNAL_SERVER_ERROR);
+            }
+            AlgoriaLOApiResponse apiResponse = resp.readEntity(AlgoriaLOApiResponse.class);
+            response.getLosId().addAll(apiResponse.getElements().stream().map(AlgoriaLOApiResponse.AlgoriaLOElement::getLosId).collect(Collectors.toList()));
+        } catch (JsonProcessingException e) {
+            throw new EwpWebApplicationException("Error fetching data: " + e.getMessage(), Response.Status.INTERNAL_SERVER_ERROR);
+        }
+
+
+        return Response.ok(response).build();
     }
 
 }
