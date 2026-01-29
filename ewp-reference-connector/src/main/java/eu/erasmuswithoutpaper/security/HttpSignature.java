dca6da8809ab534a8f49036a3d4e93e7ea0ea056
MoritzB112 <moritzbaader@gmail.com>
2026-01-29 11:52:07 +0100

    test

--- ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/security/HttpSignature.java
+++ ewp-reference-connector/src/main/java/eu/erasmuswithoutpaper/security/HttpSignature.java
@@ -193,8 +193,10 @@ public class HttpSignature {
 
             final Signer signer = new Signer(key, signature);
             Signature signed;
-            String queryParams = uri.getQuery() == null ? "" : "?" + uri.getQuery();
-            signed = signer.sign(method, uri.getPath() + queryParams, headers);
+            String queryParams = uri.getRawQuery() == null ? "" : "?" + uri.getRawQuery();
+            String path = uri.getRawPath() == null ? uri.getPath() : uri.getRawPath();
+            logger.info("Signing (request-target): {} {}", method.toLowerCase(), path + queryParams);
+            signed = signer.sign(method, path + queryParams, headers);
 
             request.header("Authorization", signed.toString());
 
